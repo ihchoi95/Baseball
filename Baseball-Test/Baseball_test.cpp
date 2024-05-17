85bc72bcf883537857261b14dd2bc4259125a7a9
ihwa.choi <ihwa.choi@samsung.com>
2024-05-17 13:56:34 +0900

    [feature] page 27

--- Baseball-Test/Baseball_test.cpp
+++ Baseball-Test/Baseball_test.cpp
@@ -1,5 +1,13 @@
-#include "pch.h"
+﻿#include "pch.h"
+#include<stdexcept>
+#include "../Baseball/Baseball.cpp"
+using namespace std;
 
 TEST(BaseballGame, TryGameTest) {
 	EXPECT_EQ(1, 1);
+}
+
+TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmached) {
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12")), length_error);
 }
--- Baseball/Baseball.cpp
+++ Baseball/Baseball.cpp
@@ -1,7 +1,11 @@
 #include<iostream>
+#include<stdexcept>
+using namespace std;
 
 class Baseball {
 public:
-
+	void guess(const string& string) {
+		throw length_error("Must be three letters.");
+	}
 };
 
